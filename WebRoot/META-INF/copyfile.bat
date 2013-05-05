15de78a60d98bad37230b3ceb7e169dc4d865776
msafin <msafin576179169>
2013-05-05 15:08:25 +0800

    Signed-off-by: msafin <msafin576179169>

---
+++ WebRoot/META-INF/copyfile.bat
@@ -0,0 +1,7 @@
+
+rd /s/q E:\apache-tomcat-6.0.26\webapps\yjgame\WEB-INF
+mkdir E:\apache-tomcat-6.0.26\webapps\yjgame\WEB-INF
+xcopy C:\Users\Administrator\Desktop\yj\yj\WEB-INF E:\apache-tomcat-6.0.26\webapps\yjgame\WEB-INF  /q/e
+ 
+startup
+pause
